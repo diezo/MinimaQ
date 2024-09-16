89d64a2c450a6cd254eb2f9d17c4c4eb60e886ef
Deepak Soni <deepaksonii@outlook.in>
2024-09-16 22:44:37 +0530

    added generation timer

--- static/home.js
+++ static/home.js
@@ -1,6 +1,11 @@
 const topicElement = document.querySelector("#topic-input");
 const difficultyElement = document.querySelector("#difficulty-input");
 const questionsCountElement = document.querySelector("#questions-count-input");
+const timerSeconds = document.querySelector("#timer-seconds");
+const timerMilliseconds = document.querySelector("#timer-milliseconds");
+const timerScreen = document.querySelector("#timer-screen");
+
+var timerInterval = null;
 
 topicElement.addEventListener("keydown", e => {
     if (e.key == "Enter") generate();
@@ -14,6 +19,59 @@ function generate()
 
     if (topic != "")
     {
+        startTimer();
         window.location = `/quiz?topic=${btoa(topic)}&difficulty=${difficulty}&count=${questionsCount}`;
     }
 }
+
+function modifyDigits(number, i)
+{
+    let text = number.toString();
+
+    if (text.length < i) return ("0" * (text.length - i)) + text;
+    else return text;
+}
+
+function startTimer()
+{
+    timerScreen.style.display = "block";
+
+    let second = 0;
+    let millisecond = 0;
+
+    let secondsUpdated = false;
+
+    timerInterval = setInterval(() => {
+        
+        if (secondsUpdated)
+        {
+            timerSeconds.classList.add("pop-animated");
+            setTimeout(() => timerSeconds.classList.remove("pop-animated"), 250);
+
+            secondsUpdated = false;
+        }
+
+        timerSeconds.innerText = modifyDigits(second, 1);
+        timerMilliseconds.innerText = modifyDigits(millisecond, 2);
+        
+        if (millisecond >= 99)
+        {
+            second++;
+            secondsUpdated = true;
+
+            millisecond = 0;
+        }
+        else millisecond++;
+
+    }, 10);
+}
+
+function finishTimer()
+{
+    timerScreen.style.display = "none";
+    if (timerInterval != null && timerInterval != undefined) clearInterval(timerInterval);
+}
+
+document.addEventListener("visibilitychange", () =>{
+    if (!document.hidden) finishTimer();
+})
